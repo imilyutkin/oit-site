81e96542171d6446c83e2b92504fb3a17ba4ee8a
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2014-09-27 01:20:54 +0300

    added tasks to gulps

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    typescript = require('typescript'),
+    notify = require('gulp-notify'),
+    del = require('del');
+
+    gulp.task('styles', function(){
+        return gulp.src('src/styles/footer.css')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('build/assets/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+    });
+
+    gulp.task('clean', function(cb) {
+        del(['build/assets/css'], cb)
+    });
+
+    gulp.task('default', ['clean'], function() {
+        gulp.start('styles');
+    });
+
